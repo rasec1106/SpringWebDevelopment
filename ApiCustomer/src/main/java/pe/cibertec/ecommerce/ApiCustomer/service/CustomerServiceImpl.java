3e17adb841c2e3526aebccb6c4eae1240b33d360
Cesar Herrera <elch.herrera@gmail.com>
2023-04-03 20:29:05 -0500

    Implement customer service

--- ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerServiceImpl.java
+++ ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerServiceImpl.java
@@ -5,6 +5,8 @@
 package pe.cibertec.ecommerce.ApiCustomer.service;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import pe.cibertec.ecommerce.ApiCustomer.dao.CustomerRepository;
 import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
 
 /**
@@ -13,14 +15,20 @@ import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
  */
 public class CustomerServiceImpl implements CustomerService{
 
+    /**
+     * This autowired annotation uses a framework to automatically create a new object when necessary
+     */
+    @Autowired // annotation for dependency injection
+    private CustomerRepository customerRepository;
+    
     @Override
     public List<Customer> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return (List<Customer>)customerRepository.findAll(); // this method is available because we are using CrudRepository
     }
 
     @Override
     public Customer findById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return customerRepository.findById(id).orElse(null);
     }
     
 }
