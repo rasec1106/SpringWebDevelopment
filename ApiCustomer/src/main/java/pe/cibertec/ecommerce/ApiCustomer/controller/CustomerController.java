e929d7e9f4a69c1311468851ddabe387ebb43516
Cesar Herrera <elch.herrera@gmail.com>
2023-04-03 21:29:54 -0500

    Add swagger and properties files

--- ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/controller/CustomerController.java
+++ ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/controller/CustomerController.java
@@ -7,6 +7,7 @@ package pe.cibertec.ecommerce.ApiCustomer.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
@@ -27,8 +28,13 @@ public class CustomerController {
         return customerService.findAll();    
     }
     
-    @GetMapping("/findById")
-    public Customer findById(Long id){
+    /**
+     * There are 2 ways of working with parameters
+     * 1. https:xxx/yy/findById?id=1 ... queryString or requestParams
+     * 2. https:xxx/yy/findById/1    ... pathVariables
+     */
+    @GetMapping("/findById/{id}") // we'll work with path variables
+    public Customer findById(@PathVariable Long id){
         return customerService.findById(id);    
     }
 }
