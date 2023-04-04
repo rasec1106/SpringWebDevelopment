809803531f49bda08e899021fd30880a8111ef3e
Cesar Herrera <elch.herrera@gmail.com>
2023-04-03 20:00:40 -0500

    Add extra plugin on POM and create CustomerService

---
+++ ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.cibertec.ecommerce.ApiCustomer.service;
+
+import java.util.List;
+import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
+
+/**
+ *
+ * @author Cesar Herrera
+ */
+public interface CustomerService {
+    public List<Customer> findAll();
+    public Customer findById(Long id);
+}
---
+++ ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerServiceImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.cibertec.ecommerce.ApiCustomer.service;
+
+import java.util.List;
+import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
+
+/**
+ *
+ * @author Cesar Herrera
+ */
+public class CustomerServiceImpl implements CustomerService{
+
+    @Override
+    public List<Customer> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
