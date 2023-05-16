c7cadbc55c0eef3797fea752f5e6c992f74533de
SUITE <user@hotmail.com>
2023-05-15 19:24:48 -0500

    Change Response type

--- ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/controller/CustomerController.java
+++ ApiCustomer/src/main/java/pe/cibertec/ecommerce/ApiCustomer/controller/CustomerController.java
@@ -6,6 +6,8 @@ package pe.cibertec.ecommerce.ApiCustomer.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,9 +30,15 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
     
+    /*
+     * We need to improve the way we handle responses.
+     * To do this we can use a Java class called ResponseEntity
+     */
+    
     @GetMapping("/findAll")
-    public List<Customer> findAll(){
-        return customerService.findAll();    
+    public ResponseEntity<List<Customer>> findAll(){
+        return new ResponseEntity<>(customerService.findAll(),
+                HttpStatus.OK);   
     }
     
     /*
