a4c4f69b97eee48e2dac3ec4b2f561ea79bfd1c1
rasec1106 <elch.herrera@gmail.com>
2023-05-21 19:14:14 -0500

    Adding annotation for intermediate table

--- ApiAuth/src/main/java/pe/cibertec/ecommerce/ApiAuth/Entity/User.java
+++ ApiAuth/src/main/java/pe/cibertec/ecommerce/ApiAuth/Entity/User.java
@@ -8,6 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import java.util.Set;
 import lombok.Data;
 
 /**
@@ -24,4 +28,13 @@ public class User {
     private String userName;
     private String email;
     private String password;
+    
+    @ManyToMany // to map the relationship
+    @JoinTable( name="User_Roles", // to map the intermediate table
+            joinColumns = @JoinColumn(name = "userid", // id for the intermediate table (for this class)
+                    referencedColumnName = "id"), // id of the main table (User)
+            inverseJoinColumns = @JoinColumn(name = "roleid", // id for the intermediate table (for the variable here below)
+                    referencedColumnName = "id") // id of the variable referenced table (Role)
+    ) 
+    private Set<Role> roles;
 }
