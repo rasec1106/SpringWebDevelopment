dfa362ff067a09b76b0576b4a4bc615f0d7bc2c9
rasec1106 <elch.herrera@gmail.com>
2023-05-21 19:05:26 -0500

    Create models

---
+++ ApiAuth/src/main/java/pe/cibertec/ecommerce/ApiAuth/Entity/User.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.cibertec.ecommerce.ApiAuth.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ *
+ * @author Cesar.Herrera
+ */
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+}
